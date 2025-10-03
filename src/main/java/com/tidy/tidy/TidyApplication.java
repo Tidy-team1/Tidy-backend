11fd33e3f0b36306663a74354610577de71b6011
ygcho <ygcho21@gmail.com>
2025-10-03 18:08:29 +0900

    init: 스프링부트 초기 세팅

---
+++ src/main/java/com/tidy/tidy/TidyApplication.java
@@ -0,0 +1,13 @@
+package com.tidy.tidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidy/tidy/TidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidy.tidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
